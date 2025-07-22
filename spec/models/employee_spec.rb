9ce1b09876586eb9f349911b656603fa16a76de3
Murilo Miranda <murilomirandamoreno@gmail.com.br>
2025-07-22 17:41:44 -0300

    Add association

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Employee, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to have_one_attached(:picture) }
+    it { is_expected.to belong_to(:company) }
   end
 end
